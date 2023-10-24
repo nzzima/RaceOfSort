17049fd640026acee7b2b7b7372568d8edf799d6
nzzima <nikita-krylov-2012@mail.ru>
2023-10-24 15:13:34 +0300

    Deleted some comments from TestSortList

--- TestSortList/Program.cs
+++ TestSortList/Program.cs
@@ -7,9 +7,6 @@ namespace TestSortList
     {
         static void Main(string[] args)
         {
-            //int[] arr = new int[] { 1, 2, -1, 15, 1000, -5, 2432, 1 };
-            //ListOfSortingAlgs.CountingSort(arr);
-            //Console.WriteLine(string.Join(", ", arr));
             BenchmarkRunner.Run<ListOfSortingBenchmark>();
         }
     }
